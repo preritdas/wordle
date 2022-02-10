791d8fca57b4341d543c13265e1a178b50f1f005
Prerit Das <preritdas@gmai.com>
2022-02-09 18:11:09 -0800

    Updated for version 1.5.34. Includes setup.py file, updated read-me, and distribution files.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.5.34'
+DESCRIPTION = 'The popular word game recreated in Python, deployable with custom answers.'
+
+def getReadMe():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+# Setting up
+setup(
+    name="wordle-python",
+    version=VERSION,
+    author="Prerit Das",
+    author_email="<preritdas@gmail.com>",
+    description=DESCRIPTION,
+    long_description = getReadMe(),
+    long_description_content_type = "text/markdown",
+    packages=find_packages(),
+    install_requires=['requests==2.27.1'],
+    keywords=['python', 'games', 'wordle', 'english', 'word games'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
