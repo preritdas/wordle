7973196ba055d4aa7d5209d0a2262f5a5b607b6f
Prerit Das <preritdas@gmail.com>
2022-02-11 01:13:59 -0800

    Fixed distribution issue in accordance with version 1.6.5. Functional.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = "1.6.4"
+VERSION = "1.6.5"
 DESCRIPTION = (
     "The popular word game recreated in Python, deployable with custom answers."
 )
