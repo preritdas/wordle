64babe51c4cb0ca506471b7dc47427ccb0a7314a
Prerit Das <preritdas@gmail.com>
2022-03-29 17:34:32 -0700

    Distributed bug fix v2.2.1.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "2.2.0"
+VERSION = "2.2.1"
 DESCRIPTION = (
     "The popular word game recreated in Python, deployable with custom answers."
 )
