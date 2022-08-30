4080d65175ea587d7430b692923f0f937cc49e45
Prerit Das <preritdas@gmail.com>
2022-08-30 16:10:53 -0400

    No dependencies required.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "2.3.3"
+VERSION = "2.3.31"
 DESCRIPTION = (
     "The popular word game recreated in Python, deployable with custom answers."
 )
@@ -19,7 +19,7 @@ setup(
     long_description=get_read_me(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=["requests==2.27.1", "DateTime==4.4"],
+    install_requires=[],
     keywords=["python", "games", "wordle", "english", "word games"],
     classifiers=[
         "Programming Language :: Python :: 3",
